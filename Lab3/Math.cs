c41ff6297297bfffe90dfd7b284255c71b4a4816
dmitry-yuzefchik <d.yuzefchik@gmail.com>
2019-09-23 10:09:12 +0300

    Finished lab3

---
+++ Lab3/Math.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab3
+{
+    public static class Math
+    {
+        public static bool IsTriangleCanExist(float a, float b, float c)
+        {
+            if ((a + b > c) && (b + c > a) && (a + c > b))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Lab3/MathTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Lab3
+{
+    [TestFixture]
+    public class MathTest
+    {
+
+        /// <summary>
+        /// Triangle 1, 1, 1 should exist
+        /// </summary>
+        [Test]
+        public void FirstTest()
+        {
+            bool expected = Math.IsTriangleCanExist(1, 1, 1);
+            Assert.IsTrue(expected);
+        }
+
+        [TestCase(200, 1, 300)]
+        public void SecondTest(float a, float b, float c)
+        {
+            bool expected = Math.IsTriangleCanExist(a, b, c);
+            Assert.IsTrue(!expected);
+        }
+
+        [TestCase(100, 200, 200, ExpectedResult = true, Description = "Triangle with sides: 100, 200, 200")]
+        [TestCase(20, 2, 30, ExpectedResult = false)]
+        [TestCase(10, 200, 3, ExpectedResult = false)]
+        [TestCase(11, 22, 33, ExpectedResult = false)]
+        [TestCase(11, 2, 35, ExpectedResult = false)]
+        [TestCase(121, 25, 73, ExpectedResult = false)]
+        [TestCase(111, 213, 322, ExpectedResult = true)]
+        [TestCase(133, 2275, 32, ExpectedResult = false)]
+        public bool TestSequence(float a, float b, float c)
+        {
+            bool expected = Math.IsTriangleCanExist(a, b, c);
+            return expected;
+        }
+    }
+}
