be8ed033789f73be9af985031c4143d50aae4a04
dmitry-yuzefchik <d.yuzefchik@gmail.com>
2019-10-23 10:53:45 +0300

    refactored webdriver

---
+++ WebDriverTest/WebDriverTest/CartPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using System.Threading;
+
+namespace WebDriverTest
+{
+    public static class CartPage
+    {
+        public static string pageUrl = "https://www.ttn.by/shop/cart?issubmit=0";
+        public static string searchInputTagId = "woocommerce-product-search-field-0";
+        public static string firstProductPath = "//*[@class='search-popup-ad']/div[2]/div[1]/a";
+        public static string addToCartButtonPath = "//*[@class='single_add_to_cart_button button alt wc-variation-selection-needed to-cart-for-ajax']";
+        public static string firstProductInCartPath = "//*[@id='cart-table']/tbody/tr[1]/td[2]/div/div/a";
+
+        public static void AddToCart(string productName, IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(MainPage.pageUrl);
+            IWebElement searchElement = driver.FindElement(By.Id(searchInputTagId));
+            SlowType(searchElement, productName);
+            Thread.Sleep(3000);
+
+            driver.FindElement(By.XPath(firstProductPath)).Click();
+            Thread.Sleep(3000);
+
+            driver.FindElement(By.XPath(addToCartButtonPath)).Click();
+            Thread.Sleep(3000);
+        }
+
+        public static string CheckAddingToCart(string productName, IWebDriver driver)
+        {
+            AddToCart(productName, driver);
+            driver.Navigate().GoToUrl(pageUrl);
+            string productTitle = driver.FindElement(By.XPath(firstProductInCartPath)).Text;
+            return productTitle;
+        }
+
+        public static void SlowType(IWebElement webElement, string text)
+        {
+            webElement.Click();
+            webElement.Clear();
+            foreach (var key in text)
+            {
+                webElement.SendKeys(key.ToString());
+                Thread.Sleep(150);
+            }
+        }
+    }
+}
--- WebDriverTest/WebDriverTest/WebDriverTest.cs
+++ WebDriverTest/WebDriverTest/WebDriverTest.cs
@@ -1,77 +1,41 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 
 namespace WebDriverTest
 {
     class WebDriverTest
     {
-        private string _homeURL;
         private IWebDriver _driver;
+        private string _productName;
 
         [SetUp]
         public void Setup()
         {
-            _homeURL = "https://www.ttn.by/";
             _driver = new ChromeDriver();
+            _productName = "Смартфон Xiaomi Mi A1 4GB/64GB (черный)";
         }
 
         [Test]
         public void CheckSearchField()
         {
-            string product = "Смартфон Xiaomi Mi A1 4GB/64GB (черный)";
-            string searchTagId = "woocommerce-product-search-field-0";
-
-            _driver.Navigate().GoToUrl(_homeURL);
-            IWebElement searchElement = _driver.FindElement(By.Id(searchTagId));
-            SlowType(searchElement, product);
-
-            Thread.Sleep(3000);
-            string phoneTitle = _driver.FindElement(By.XPath("//*[@class='search-popup-ad']/div[2]/div[1]/a/div[1]")).Text;
-            Assert.IsTrue(phoneTitle == product);
+            string searchFieldValue = MainPage.GetSearchFieldValue(_productName, _driver);
+            bool expected = searchFieldValue == _productName;
+            Assert.IsTrue(expected);
         }
 
         [Test]
         public void CheckAddingToCart()
         {
-            string product = "Смартфон Xiaomi Mi A1 4GB/64GB (черный)";
-            string searchTagId = "woocommerce-product-search-field-0";
-
-            _driver.Navigate().GoToUrl(_homeURL);
-            IWebElement searchElement = _driver.FindElement(By.Id(searchTagId));
-            SlowType(searchElement, product);
-            
-            Thread.Sleep(3000);
-            _driver.FindElement(By.XPath("//*[@class='search-popup-ad']/div[2]/div[1]/a")).Click();
-            Thread.Sleep(3000);
-            
-            _driver.FindElement(By.XPath("//*[@class='single_add_to_cart_button button alt wc-variation-selection-needed to-cart-for-ajax']")).Click();
-            Thread.Sleep(3000);
-            
-            _driver.Navigate().GoToUrl("https://www.ttn.by/shop/cart?issubmit=0");
-            string phoneTitle = _driver.FindElement(By.XPath("//*[@id='cart-table']/tbody/tr[1]/td[2]/div/div/a")).Text;
-            Assert.IsTrue(phoneTitle == product);
+            string productTitle = CartPage.CheckAddingToCart(_productName, _driver);
+            bool expected = productTitle == _productName;
+            Assert.IsTrue(expected);
         }
 
         [TearDown]
         public void TearDown()
         {
             _driver.Close();
-        }
-
-        private void SlowType(IWebElement webElement, string text)
-        {
-            webElement.Click();
-            webElement.Clear();
-            foreach (var key in text)
-            {
-                webElement.SendKeys(key.ToString());
-                Thread.Sleep(150);
-            }
         }
     }
 }
